d370345777cbdae15ba2279186220c5273e6479c
Arpan Ghosal <arpanghosal0160@gmail.com>
2025-05-24 20:37:05 +0530

    tweaked middleware

--- lib/auth.ts
+++ lib/auth.ts
@@ -67,5 +67,5 @@ export const authOptions: NextAuthOptions = {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60,
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.AUTH_SECRET,
 };

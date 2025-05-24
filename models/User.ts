ac3e5cbe234a1307841e25ee7b3147244a8940d0
Arpan Ghosal <arpanghosal0160@gmail.com>
2025-05-24 10:23:26 +0530

    route debugging

--- models/User.ts
+++ models/User.ts
@@ -27,7 +27,7 @@ const userSchema = new Schema<IUser>(
 )
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
+    if (this.isModified("password")) {
         this.password = await bcrypt.hash(this.password, 10);
     }
     next(); 
